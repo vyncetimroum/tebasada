388339effe65ef531c2b5ab27d6a9d6d10008e62
vyncetimroum <henrydahles@outlook.com>
2025-03-09 12:25:47 +0100

    Create idso.lua

---
+++ idso.lua
@@ -0,0 +1,15 @@
+return {
+  7726934593, -- gxslighted
+  7870300601, -- fxIIacy
+  8038030326, -- ervcommunity
+  8068753883, -- lmmortalityinthegame
+  7217701136, -- Fizzlett69
+  3247681785, -- BinBroPro
+  3162556171, -- IchNichtHi
+  5426329723, -- NLS_BlackiAfrica
+  7137329836, -- kirito007026
+  7471872501, -- KAI123337909
+  7385645405, -- dun_alt5
+  643863047, -- SamrantaOMG
+  1677614105, -- RHAM3K
+}
