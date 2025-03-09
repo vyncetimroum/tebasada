3a4f9e07c3de4a8fd9241531377deb16a2ff41a8
vyncetimroum <henrydahles@outlook.com>
2025-03-09 12:27:52 +0100

    Create idsb.lua

---
+++ idsb.lua
@@ -0,0 +1,9 @@
+return {
+  5735050322, -- god_elmo9
+  3034004536, -- Ravman_146
+  3140640792, -- Gulvlist
+  7379953841, -- Wassupdidiyudi
+  7887926378, -- BGH_SUPERILLIONALT
+  7926027644, -- BGH_SUPERILLIONV2
+  7189848965, -- script9452
+}
